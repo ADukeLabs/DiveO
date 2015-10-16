2e8dfb7f77418048470a4db5ed31f98fe07c4c57
ADukeLabs <d.alex.duke@gmail.com>
2015-10-16 17:48:28 -0400

    Image Processor Class Added to Services. Replaces byte array code from Create Diver method

---
+++ DiveO/DiveO/Services/ImageProcessor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace DiveO.Services
+{
+    public class ImageProcessor
+    {
+        public byte[] ImageToByteArray(HttpPostedFileBase file)
+        {
+            byte[] result;
+            using (MemoryStream ms = new MemoryStream())
+            {
+                file.InputStream.CopyTo(ms);
+                result = ms.GetBuffer();
+            }
+
+            return result;
+        }
+    }
+}
