c4b66ddd51c72b1f8dec442198d950b7a7a6009a
ADukeLabs <d.alex.duke@gmail.com>
2015-11-07 16:54:32 -0500

    Google Map API added to project. Map now part of Dive Details page, but not working. Custom made view for Dive Details built

---
+++ DiveO/DiveO/Models/DivePhoto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+
+namespace DiveO.Models
+{
+    public class DivePhoto
+    {
+        public int Id { get; set; }
+        public byte[][] Photos { get; set; }
+    }
+}
---
+++ DiveO/DiveO/Scripts/MapJs.js
@@ -0,0 +1,8 @@
+﻿function initialize() {
+    var mapOptions = {
+        center: new google.maps.LatLng(0, 0),
+        zoom: 4
+    };
+    var map = new google.maps.Map(document.getElementById("mapDiv"), mapOptions);
+}
+google.maps.event.addDomListener(window, "load", initialize);
