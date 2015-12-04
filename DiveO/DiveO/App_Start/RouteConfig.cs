396f74b8421ee59f8522dad62bae29d901ee4810
ADukeLabs <d.alex.duke@gmail.com>
2015-12-03 19:27:39 -0500

    Feed Method and View added to Home Controller

--- DiveO/DiveO/App_Start/RouteConfig.cs
+++ DiveO/DiveO/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace DiveO
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Feed", id = UrlParameter.Optional }
             );
         }
     }
--- DiveO/DiveO/Controllers/HomeController.cs
+++ DiveO/DiveO/Controllers/HomeController.cs
@@ -3,14 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DiveO.Models;
+using DiveO.ViewModels;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace DiveO.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        ApplicationDbContext db = new ApplicationDbContext();
+        protected UserManager<ApplicationUser> UserManager { get; set; }
+
+        public HomeController()
         {
-            return View();
+            this.UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(this.db));
+        } 
+
+        [Authorize]
+        public ActionResult Feed()
+        {
+            DiverViewModel dvm = new DiverViewModel();
+
+            var id = User.Identity.GetUserId();
+            var diver = db.Divers.Find(id);
+            dvm.Name = diver.Name;
+            dvm.ProfilePic = diver.ProfilePic;
+            dvm.Dives = diver.DiveLog;
+            return View(dvm);
         }
 
         public ActionResult About()
