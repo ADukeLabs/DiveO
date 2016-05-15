63716562350f753c73c6d7f03578f616efd73776
ADukeLabs <d.alex.duke@gmail.com>
2016-05-15 19:47:25 -0400

    Photo controller and GetPhoto actionlink added. Needs further updating

---
+++ DiveO/DiveO/Controllers/PhotoController.cs
@@ -0,0 +1,25 @@
+﻿using DiveO.Models;
+using DiveO.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DiveO.Controllers
+{
+    public class PhotoController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Photo
+        public ActionResult GetPhoto(int? id)
+        {
+            DiveViewModel dvm = new DiveViewModel();
+            dvm.Photos = db.DivePhotos.Find(id);
+            return View();
+        }
+
+
+    }
+}
