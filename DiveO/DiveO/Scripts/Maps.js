96788528ca2da56b875d1d0b1fab8d597af79939
ADukeLabs <d.alex.duke@gmail.com>
2016-05-24 20:36:04 -0400

    Google maps code placed into Maps.js file. Code for geolocations added from documentation. Not working yet.

---
+++ DiveO/DiveO/Scripts/Maps.js
@@ -0,0 +1,21 @@
+﻿var initialLocation;
+var browserSupportFlag = new Boolean();
+
+function initialize() {
+    var mapOptions = {
+        center: new google.maps.LatLng(0, 0),
+        zoom: 4
+    };
+    var map = new google.maps.Map(document.getElementById("mapDiv"), mapOptions);
+
+    if (navigator.geolocation) {
+        browserSupportFlag = true;
+        navigator.geolocation.getCurrentPosition(function (position) {
+            initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+            map.setCenter(initialLocation);
+        }, function () {
+            handleNoGeolocation(browserSupportFlag);
+        });
+    }
+}
+google.maps.event.addDomListener(window, "load", initialize);
