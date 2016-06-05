f888f6a92d606719ada6497341a1302f9e4d7844
ADukeLabs <d.alex.duke@gmail.com>
2016-06-05 14:04:43 -0400

    Google Maps Geolocation API added to project

--- DiveO/DiveO/Scripts/Maps.js
+++ DiveO/DiveO/Scripts/Maps.js
@@ -8,14 +8,14 @@ function initialize() {
     };
     var map = new google.maps.Map(document.getElementById("mapDiv"), mapOptions);
 
-    //if (navigator.geolocation) {
-    //    browserSupportFlag = true;
-    //    navigator.geolocation.getCurrentPosition(function (position) {
-    //        initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-    //        map.setCenter(initialLocation);
-    //    }, function () {
-    //        handleNoGeolocation(browserSupportFlag);
-    //    });
-    //}
+    if (navigator.geolocation) {
+        browserSupportFlag = true;
+        navigator.geolocation.getCurrentPosition(function (position) {
+            initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+            map.setCenter(initialLocation);
+        }, function () {
+            handleNoGeolocation(browserSupportFlag);
+        });
+    }
 }
 google.maps.event.addDomListener(window, "load", initialize);
