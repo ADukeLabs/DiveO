ef476cca0267d06a88c8650a706a0720965ddbaa
ADukeLabs <d.alex.duke@gmail.com>
2016-06-05 11:56:32 -0400

    MapPoint class added to Model Attributes

---
+++ DiveO/DiveO/Models/Model Attributes/MapPoint.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DiveO.Models.Model_Attributes
+{
+    public class MapPoint
+    {
+        public float Latitude { get; set; }
+        public float Longitude { get; set; }
+    }
+}
